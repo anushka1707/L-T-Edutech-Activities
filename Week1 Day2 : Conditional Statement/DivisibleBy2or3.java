e45ff6e6c278ef306ba39c2c526913816c17ffb7
anushka1707 <83497015+anushka1707@users.noreply.github.com>
2023-03-10 00:09:05 +0530

    Create DivisibleBy2or3.java

---
+++ Week1 Day2 : Conditional Statement/DivisibleBy2or3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        
+            // Fill your code here
+            Scanner sc=new Scanner(System.in);
+            int Num=sc.nextInt();
+            if(Num%2==0 || Num%3==0)
+            System.out.println("yes");
+            else
+            System.out.println("no");
+
+        
+     }
+}
