d09992047802acd3899795af64dc47004a47a641
anushka1707 <83497015+anushka1707@users.noreply.github.com>
2023-03-10 00:08:00 +0530

    Create findEvenOrOdd.java

---
+++ Week1 Day2 : Conditional Statement/findEvenOrOdd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        
+            // Fill your code here
+           Scanner sc= new Scanner(System.in);
+           int Num=sc.nextInt();
+           if(Num%2==0)
+           System.out.println(Num+" is an even number");
+           else
+           System.out.println(Num+" is an odd number");
+
+     }
+}
