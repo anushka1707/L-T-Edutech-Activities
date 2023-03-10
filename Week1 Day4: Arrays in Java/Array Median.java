9d865b89e5b6dc103f5c28523cc553b5928b7eaf
anushka1707 <83497015+anushka1707@users.noreply.github.com>
2023-03-10 22:29:00 +0530

    Create Array Median.java

---
+++ Week1 Day4: Arrays in Java/Array Median.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+	Scanner scn= new Scanner(System.in);
+	System.out.println("Enter the  number of elements in the array");
+	int n= scn.nextInt();
+	System.out.println("Enter the elements in the array");
+	int[] arr= new int[n];
+	for(int i=0;i<=n-1;i++){
+		arr[i]= scn.nextInt();
+	}
+	Arrays.sort(arr);
+	double median;
+	if(n%2==0){
+		median= (double) (arr[n/2] + arr[n/2-1])/2;
+	}
+	else{
+		median= arr[n/2];
+	}
+System.out.printf("The median of the array is %.2f", median);
+	}
+
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Sec
